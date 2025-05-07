6f23587205d046b5ed68a9d39637698a6f6913d6
bean <dbean1515@gmail.com>
2025-05-06 21:16:48 -0500

    backup changes

--- app/drinks/add/page.js
+++ app/drinks/add/page.js
@@ -5,11 +5,14 @@ import { ToastContainer, toast } from 'react-toastify';
 import useIngredients from "../../hooks/useIngredients";
 import loadingStatus from "../../helpers/loadingStatus";
 import LoadingIndicator from "../../components/LoadingIndicator";
+import ShowMessage from "../../components/ShowMessage";
 
 const AddDrink = () => {  
     const [formData, setFormData] = useState({id: 0, name: '', price: 0, ingredients: []});
-    const { ingredients, loadingState  } = useIngredients();     
-
+    const { ingredients, loadingState  } = useIngredients();  
+    
+    const [message, setMessage] = useState(''); //I need a validation message  
+    
     if (loadingState !== loadingStatus.loaded){
         return <LoadingIndicator loadingState={loadingState} />;
     }
@@ -24,7 +27,8 @@ const AddDrink = () => {
       }));      
     };  
 
-    const addControlPair = () => {       
+    const addControlPair = () => {  
+        setMessage('');     
         const updatedFormData = {...formData};      
         const { ingredients } = updatedFormData; 
         var updatedIngredients = [...ingredients, { id: "", quantity: "", name: "" }]
@@ -41,6 +45,7 @@ const AddDrink = () => {
       };
     
       const handleDropdownChange = (index, event) => {
+        setMessage('')
         const updatedFormData = {...formData};
         const { ingredients } = updatedFormData;
         ingredients[index].id = event.target.value; 
@@ -55,29 +60,100 @@ const AddDrink = () => {
         updatedFormData.ingredients = ingredients; //what about mutability here?
         setFormData(updatedFormData);
       };
+            
+      const checkIngredientSelections = (selectedIngredients) => {
+        let areSelectionsValid = true;
+        
+        selectedIngredients.forEach(function (item) {
+          if (item.id === '') {            
+            areSelectionsValid = false;           
+          }
+        });
+        return areSelectionsValid;
+      }
+
+      const getDuplicatedIngredients = (selectedIngredients) => {
+        const seen = new Set();
+        const duplicates = new Set();             
+
+        selectedIngredients.forEach(function(item) {            
+            if (seen.has(item.id)) {              
+              duplicates.add(item.id.toString());
+            } else {
+              seen.add(item.id.toString());        
+            }     
+          })
+
+          return [...duplicates]; 
+       } 
+
+      const checkIngredientsForDuplicates = (selectedIngredients) => {
+          let hasDuplicates = false;
+          let dupes = getDuplicatedIngredients(selectedIngredients);
+          console.log(dupes);
+      
+          const isEmpty = dupes.length === 0; // true
+          if (!isEmpty) {
+            hasDuplicates = true;
+          } 
+          return hasDuplicates;
+      }
     
     const handleSubmit = (event) => {
       event.preventDefault();   
       console.log('Form submitted:', formData);   
       //add validation//
+      //todo: I need the validation in the edit drink page too.
       //todo replace all vars with const in react and angular projects.
-      const { ingredients } = formData;
-      console.log(ingredients); 
+      //const { ingredients } = formData;
+      const { ingredients: selectedIngredients } = formData; 
+      console.log('log ingredients...')
+      console.log(selectedIngredients); 
+      
+      if(selectedIngredients.length === 0){
+        console.log('empty ingredient array');
+        setMessage('one ingredient is required.');
+        return;
+      }
+
+      //todo: extract this to a method...
+      // let areSelectionsValid = true;
+      // ingredients.forEach(function(item) {
+      //   if(item.id === ''){
+      //     console.log('invalid selection...');
+      //     areSelectionsValid = false;
+      //     setMessage('Ingredient selection is not valid.');        
+      //   }
+      // })
+
+      let areSelectionsValid = checkIngredientSelections(selectedIngredients);
+
+      if(!areSelectionsValid){
+        setMessage('Ingredient selection is not valid.');        
+        return;
+      }
+
+      const hasDuplicates = checkIngredientsForDuplicates(selectedIngredients);
+      if(hasDuplicates){     
+        setMessage('You have a duplicate ingredient selection. Please change one.');  
+        return;
+      }
+      
       //add validation//
         // Call the API to add the drink
         const url = 'https://localhost:7070/api/Drinks'; // todo: get base url from config
-      //   createData(url, formData).then(
-      //   function(value) {
-      //       console.log(value);
-      //       toast.success('Item created successfully!');
-      //       //reset form
-      //       setFormData({id: 0, name: '', price: 0, ingredients: []});
-      //   },
-      //   function(error) {        
-      //       console.log(error);   
-      //       toast.error("error occurred creating ingredient.");   
-      //   }
-      // );  
+        createData(url, formData).then(
+        function(value) {
+            console.log(value);
+            toast.success('Item created successfully!');
+            //reset form
+            setFormData({id: 0, name: '', price: 0, ingredients: []});
+        },
+        function(error) {        
+            console.log(error);   
+            toast.error("error occurred creating ingredient.");   
+        }
+      );  
     };
 
     return (    
@@ -128,6 +204,8 @@ const AddDrink = () => {
                 value={control.text}
                 onChange={(e) => handleTextChange(index, e)}
                 placeholder="Enter a quantity"
+                required
+                min="0"
                 />
             </div>
             <div className="col-md-3">
@@ -153,11 +231,15 @@ const AddDrink = () => {
             <button type="submit" className="btn btn-primary">Submit</button>
             </div>
         </div>
-        </form>
+        
+        {message && ( <ShowMessage message={message}/> )}     
+        </form>        
          <ToastContainer />
        </>
   );
 };
 
 export default AddDrink;
+
+
 
